504475a2ab74719b60b4e02cdbdcf39ac7f2c6a0
ahmedherhesh <ahmedherhesh3@gmail.com>
2023-11-27 22:37:53 +0200

    سخةث صخقن

--- app/Http/Controllers/ViolationController.php
+++ app/Http/Controllers/ViolationController.php
@@ -15,18 +15,18 @@ class ViolationController extends Controller
     public function store(ViolationRequest $request)
     {
         Violation::create($request->all());
-        return response()->json(['status' => 200, 'msg' => 'تم اضافة الوحدة']);
+        return response()->json(['status' => 200, 'msg' => 'تم اضافة المخالفة']);
     }
 
     public function update(ViolationRequest $request, Violation $violation)
     {
         $violation->update($request->all());
-        return response()->json(['status' => 200, 'msg' => 'تم تحديث الوحدة']);
+        return response()->json(['status' => 200, 'msg' => 'تم تحديث المخالفة']);
     }
 
     public function destroy(Violation $violation)
     {
         $violation->delete();
-        return response()->json(['status' => 200, 'msg' => 'تم حذف الوحدة']);
+        return response()->json(['status' => 200, 'msg' => 'تم حذف المخالفة']);
     }
 }
