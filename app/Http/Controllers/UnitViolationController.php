77a8e78b11bcc1ec5ab06d0671f8b3b2a0b25cee
ahmedherhesh <ahmedherhesh3@gmail.com>
2023-12-06 17:04:36 +0200

    some work

--- app/Http/Controllers/UnitViolationController.php
+++ app/Http/Controllers/UnitViolationController.php
@@ -48,28 +48,29 @@ class UnitViolationController extends MasterController
     {
         $unitViolation = UnitViolation::find($id);
         if (!$unitViolation)
-            return response()->json(['msg' => 'عفواً هذه المخالفة غير متوفرة']);
+            return response()->json(['status' => 400, 'msg' => 'عفواً هذه المخالفة غير متوفرة']);
 
         if ($unitViolation->cant_edit_at <= Carbon::now() && !$this->isAdmin())
-            return response()->json(['msg' => 'انتهى وقت التعديل على هذه المخالفة']);
+            return response()->json(['status' => 400, 'msg' => 'انتهى وقت التعديل على هذه المخالفة']);
 
         $unitViolation->update($request->only('count'));
-        return response()->json(['msg' => 'تم تحديث المخالفة']);
+        return response()->json(['status' => 200, 'msg' => 'تم تحديث المخالفة']);
     }
 
     public function destroy($id)
     {
         $unitViolation = UnitViolation::whereId($id);
-        if ($unitViolation->first())
-            return response()->json(['msg' => 'عفواً هذه المخالفة غير متوفرة']);
+        if (!$unitViolation->first())
+            return response()->json(['status' => 400, 'msg' => 'عفواً هذه المخالفة غير متوفرة']);
 
         if ($unitViolation->first()->cant_edit_at <= Carbon::now() && !$this->isAdmin())
-            return response()->json(['msg' => 'انتهى وقت التعديل على هذه المخالفة']);
+            return response()->json(['status' => 400, 'msg' => 'انتهى وقت التعديل على هذه المخالفة']);
 
         if (!$this->isAdmin())
             $unitViolation = $unitViolation->whereUserId($this->user()->id);
         $unitViolation = $unitViolation->first();
         if ($unitViolation)
             $unitViolation->delete();
+        return response()->json(['status' => 200, 'msg' => 'تم حذف المخالفة']);
     }
 }
